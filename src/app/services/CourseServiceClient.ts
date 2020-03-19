eedd258998444244c6c6024bf9039286199abf8b
songjiuliu <songjiuliu@hotmail.com>
2020-03-19 16:40:25 -0400

    fix heroku

--- src/app/services/CourseServiceClient.ts
+++ src/app/services/CourseServiceClient.ts
@@ -3,11 +3,11 @@ import {Injectable} from '@angular/core';
 @Injectable()
 export class CourseServiceClient {
   findAllCourses = () =>
-    fetch('http://wbdv-generic-server.herokuapp.com/api/songjiu/courses')
+    fetch('https://wbdv-generic-server.herokuapp.com/api/songjiu/courses')
       .then(response => response.json());
   createCourse = async (course) =>
   {
-    const response = await fetch('http://wbdv-generic-server.herokuapp.com/api/songjiu/courses', {
+    const response = await fetch('https://wbdv-generic-server.herokuapp.com/api/songjiu/courses', {
       method: "POST",
       body: JSON.stringify(course),
       headers: {
@@ -17,25 +17,25 @@ export class CourseServiceClient {
     return await response.json()
   }
 deleteCourse = async (courseId) => {
-    const response = await fetch(`http://wbdv-generic-server.herokuapp.com/api/songjiu/courses/${courseId}`, {
+    const response = await fetch(`https://wbdv-generic-server.herokuapp.com/api/songjiu/courses/${courseId}`, {
       method: 'DELETE'
     })
     return await response.json()
   }
   findCourseById = (cid) =>
-    fetch('http://wbdv-generic-server.herokuapp.com/api/songjiu/courses/'+cid)
+    fetch('https://wbdv-generic-server.herokuapp.com/api/songjiu/courses/'+cid)
       .then(response => response.json());
   findModuleById = (mid) =>
-    fetch('http://wbdv-generic-server.herokuapp.com/api/songjiu/modules/'+mid)
+    fetch('https://wbdv-generic-server.herokuapp.com/api/songjiu/modules/'+mid)
       .then(response => response.json());
   findModulesForCourse = (cid) =>
-    fetch('http://wbdv-generic-server.herokuapp.com/api/songjiu/courses/'+cid+'/modules')
+    fetch('https://wbdv-generic-server.herokuapp.com/api/songjiu/courses/'+cid+'/modules')
       .then(response => response.json());
   findLessonsForModule = (mid) =>
-    fetch('http://wbdv-generic-server.herokuapp.com/api/songjiu/modules/'+mid+'/lessons')
+    fetch('https://wbdv-generic-server.herokuapp.com/api/songjiu/modules/'+mid+'/lessons')
       .then(response => response.json());
   findTopicsForLesson= (lid) =>
-    fetch('http://wbdv-generic-server.herokuapp.com/api/songjiu/lessons/'+lid+'/topics')
+    fetch('https://wbdv-generic-server.herokuapp.com/api/songjiu/lessons/'+lid+'/topics')
       .then(response => response.json());
 
 }
